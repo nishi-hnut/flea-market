16f4244f201ea3eaa07d3ebcaf86611877f911b8
nishi <nishi.hnut@gmail.com>
2024-02-26 16:31:12 +0900

    課題3修正

--- app/components/ItemBlock.tsx
+++ app/components/ItemBlock.tsx
@@ -1,22 +1,32 @@
-import React from "react";
+import React, { FC } from "react";
 
-interface ItemProps {
+type Item = {
   id: string;
   title: string;
   description?: string;
   imgUrl: string;
   price: number;
-}
+};
+type Goods = {
+  items: Item[];
+};
 
-const ItemBlock = ({ id, title, description, imgUrl, price }: ItemProps) => {
+const ItemBlock: React.FC<Goods> = ({ items }) => {
   return (
-    <div className="border p-4">
-      <div key={id} className="w-full h-64 bg-gray-400 relative">
-        <img className="w-full h-full object-cover absolute" src={imgUrl} />
-      </div>
-      <p className="mt-2">{title}</p>
-      <p>{description}</p>
-      <p>{price}</p>
+    <div className="grid grid-cols-3 gap-4">
+      {items.map((item) => (
+        <div className="border p-4">
+          <div key={item.id} className="w-full h-64 bg-gray-400 relative">
+            <img
+              className="w-full h-full object-cover absolute"
+              src={item.imgUrl}
+            />
+          </div>
+          <p className="mt-2">{item.title}</p>
+          <p>{item.description}</p>
+          <p>{item.price}</p>
+        </div>
+      ))}
     </div>
   );
 };
--- app/items/page.tsx
+++ app/items/page.tsx
@@ -3,6 +3,57 @@ import Link from "next/link";
 import ItemBlock from "../components/ItemBlock";
 
 export default function Home() {
+  const items = [
+    {
+      id: "1",
+      title: "レンジ",
+      description: "レンジです",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/06/03/13/44/bird-8037744_1280.jpg",
+      price: 100,
+    },
+    {
+      id: "2",
+      title: "冷蔵庫",
+      description: "冷蔵庫です",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/10/13/08/32/bird-8312424_1280.jpg",
+      price: 200,
+    },
+    {
+      id: "3",
+      title: "炊飯器",
+      description: "炊飯器ですです",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/05/11/15/14/honey-bee-7986647_1280.jpg",
+      price: 300,
+    },
+    {
+      id: "1",
+      title: "レンジ",
+      description: "レンジです",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/06/03/13/44/bird-8037744_1280.jpg",
+      price: 100,
+    },
+    {
+      id: "2",
+      title: "冷蔵庫",
+      description: "冷蔵庫です",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/10/13/08/32/bird-8312424_1280.jpg",
+      price: 200,
+    },
+    {
+      id: "3",
+      title: "炊飯器",
+      description: "炊飯器ですです",
+      imgUrl:
+        "https://cdn.pixabay.com/photo/2023/05/11/15/14/honey-bee-7986647_1280.jpg",
+      price: 300,
+    },
+  ];
+
   return (
     <main>
       <div className="w-full bg-red-100">
@@ -14,15 +65,7 @@ export default function Home() {
         </div>
       </div>
       <div className="max-w-5xl mx-auto my-8 p-4">
-        <div className="grid grid-cols-3 gap-4">
-          <ItemBlock
-            id="1"
-            title="鳥"
-            description="鳥です"
-            price={100}
-            imgUrl="https://cdn.pixabay.com/photo/2017/02/07/16/47/kingfisher-2046453_1280.jpg"
-          />
-        </div>
+        <ItemBlock items={items} />
       </div>
     </main>
   );
