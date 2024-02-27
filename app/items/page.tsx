077cab754487c7e8b760b63e2779663b26bf89e2
nishi <nishi.hnut@gmail.com>
2024-02-27 14:23:47 +0900

    課題5

--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,67 +1,26 @@
 "use client";
-
 import Image from "next/image";
 import Link from "next/link";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  const items = [
-    {
-      id: "1",
-      title: "レンジ",
-      description: "レンジです",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/06/03/13/44/bird-8037744_1280.jpg",
-      price: 100,
-    },
-    {
-      id: "2",
-      title: "冷蔵庫",
-      description: "冷蔵庫です",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/10/13/08/32/bird-8312424_1280.jpg",
-      price: 200,
-    },
-    {
-      id: "3",
-      title: "炊飯器",
-      description: "炊飯器です",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/05/11/15/14/honey-bee-7986647_1280.jpg",
-      price: 300,
-    },
-    {
-      id: "1",
-      title: "レンジ",
-      description: "レンジです",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/06/03/13/44/bird-8037744_1280.jpg",
-      price: 100,
-    },
-    {
-      id: "2",
-      title: "冷蔵庫",
-      description: "冷蔵庫です",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/10/13/08/32/bird-8312424_1280.jpg",
-      price: 200,
-    },
-    {
-      id: "3",
-      title: "炊飯器",
-      description: "炊飯器ですです",
-      imgUrl:
-        "https://cdn.pixabay.com/photo/2023/05/11/15/14/honey-bee-7986647_1280.jpg",
-      price: 300,
-    },
-  ];
+  const [posts, setPosts] = useState();
+
+  // useEffect(() => {
+  //   fetch("/api/items")
+  //     .then((res) => res.json())
+  //     .then((data) => {
+  //       setPosts(data);
+  //     });
+  // }, []);
 
   useEffect(() => {
     async function fetchItems() {
       try {
         const response = await fetch("/api/items");
-        const data = await response.json();
-        console.log(data);
+        const dataj = await response.json();
+        setPosts(dataj.data);
+        console.log(dataj);
       } catch (error) {
         console.error("Fetch error:", error);
       }
@@ -81,7 +40,7 @@ export default function Home() {
       </div>
       <div className="max-w-5xl mx-auto my-8 p-4">
         <div className="grid grid-cols-3 gap-4">
-          {items.map((item) => (
+          {posts.map((item) => (
             <div className="border p-4">
               <div key={item.id} className="w-full h-64 bg-gray-400 relative">
                 <img className="w-full h-full object-cover" src={item.imgUrl} />
