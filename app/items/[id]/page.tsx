78230ddcf1829b47459eb0b3be10e97541f6844c
nishi <nishi.hnut@gmail.com>
2024-02-28 10:53:36 +0900

    商品詳細画面のUI作成

--- app/items/[id]/page.tsx
+++ app/items/[id]/page.tsx
@@ -1,7 +1,47 @@
-import React from "react";
+import Link from "next/link";
+import React, { useState } from "react";
 
 const page = () => {
-  return <div>こんにちは</div>;
+  return (
+    <main>
+      <div className="w-full border-b-2 sticky top-0 z-10 bg-white">
+        <div className="flex justify-between max-w-7xl mx-auto p-4 text-center">
+          <h2 className="text-xl font-bold p-2">メルカリ</h2>
+          <ul className="flex p-2 ml-3">
+            <li className="pr-5">ログイン</li>
+            <li className="pr-5">新規登録</li>
+            <li>
+              <Link href="/pages" className=" p-2">
+                出品
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="flex max-w-5xl mx-auto container mt-6">
+        <div className="w-3/5 h-3/5">
+          <img
+            className="w-full h-full object-cover"
+            src="https://cdn.pixabay.com/photo/2023/06/03/13/44/bird-8037744_1280.jpg"
+          />
+        </div>
+
+        <div className="ml-11 w-2/5">
+          <div className="text-2xl font-bold">
+            日立 コンパクト ヘルシーシェフ 過熱水蒸気オーブンレンジ MRO-FS7
+          </div>
+          <div className="text-xl mt-5">¥5,800</div>
+          <button className="bg-red-500 text-white w-full py-3 rounded mt-10 hover:bg-red-400">
+            購入手続きへ
+          </button>
+          <h2 className="text-2xl font-bold text-gray-700 mt-10">商品の説明</h2>
+
+          <h2 className="mt-5">レンジです</h2>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default page;
--- app/items/page.tsx
+++ app/items/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 
-import Image from "next/image";
 import Link from "next/link";
-import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 
 type Item = {
@@ -32,14 +30,21 @@ export default function Home() {
 
   return (
     <main>
-      <div className="w-full bg-red-100">
-        <div className="flex justify-between bg-blue-200 max-w-5xl mx-auto p-4">
-          <h2 className="text-xl font-bold bg-yellow-200 p-2">メルカリ</h2>
-          <Link href="/pages" className="bg-green-200 p-2">
-            出品
-          </Link>
+      <div className="w-full border-b-2 sticky top-0 z-10 bg-white">
+        <div className="flex justify-between max-w-7xl mx-auto p-4 text-center">
+          <h2 className="text-xl font-bold p-2">メルカリ</h2>
+          <ul className="flex p-2 ml-3">
+            <li className="pr-5">ログイン</li>
+            <li className="pr-5">新規登録</li>
+            <li>
+              <Link href="/pages" className=" p-2">
+                出品
+              </Link>
+            </li>
+          </ul>
         </div>
       </div>
+
       <div className="max-w-5xl mx-auto my-8 p-4">
         <div className="grid grid-cols-3 gap-4">
           {items.map((item) => (
